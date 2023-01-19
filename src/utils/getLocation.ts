9c2a49b6fa7c8ca3bcc8272672e03797e72fa2b3
Prz-mek <kwasniewski.pj@gmail.com>
2023-01-19 18:11:19 +0100

    attribution

--- src/utils/getLocation.ts
+++ src/utils/getLocation.ts
@@ -1,7 +1,25 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Localization from "expo-localization";
 
-const LANGUAGE_KEY = "lang_setings";
+/**
+ * This part of software uses code form other software attributed below.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2021 Katsiaryna Lupachova
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+const LANGUAGE_KEY = "lang_settings";
 
 export default {
     type: "languageDetector",
@@ -10,15 +28,15 @@ export default {
     detect: async function (callback: (lang: string) => void) {
         try {
             // I cannot use storage utiles becouse fof the callback. 
-            await AsyncStorage.getItem(LANGUAGE_KEY).then((language) => {
-                if (language) {
-                    return callback(language);
+            await AsyncStorage.getItem(LANGUAGE_KEY).then((lang) => {
+                if (lang) {
+                    return callback(lang);
                 } else {
                     return callback(Localization.locale);
                 }
             });
         } catch (error) {
-            console.log("Geting localization", error);
+            console.log(error);
         }
     },
     cacheUserLanguage: async function (language: string) {
